4fe284197d051bf9075a50ccf33cfed2fb1fc99a
Nurujjaman329 <mdnurujjaman329@gmail.com>
2023-09-21 08:32:37 +0600

    first commit

---
+++ get_x_practise/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get_x_practise/mainpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: HomePage(),
+    );
+  }
+}
